884535cf3aa71bf6883767201073438df41e7d0d
Shayan Irfan <ishayan767@gmail.com>
2023-08-26 06:59:23 +0500

    fix: deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import "./LoadEnv.js"; // Must be the first import
 import app from "./server.js";
 import mongoConnection from "./config/db.js";
-import { createServer } from "http";
-import { webSocketConnection } from "./config/socketConnection.js";
+// import { createServer } from "http";
+// import { webSocketConnection } from "./config/socketConnection.js";
 
 /**
  * Port at which server will run
  */
 const port = Number(process.env.PORT || 5000);
-const httpServer = createServer(app);
-const sio = webSocketConnection(httpServer);
+// const httpServer = createServer(app);
+// const sio = webSocketConnection(httpServer);
 
 /**
  * Connecting to Database
@@ -20,12 +20,12 @@ await mongoConnection();
  * Starting the server
  * @param port Port at which server will run
  */
-const server = httpServer.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server Running on Port: http://localhost:${port}`);
 });
 
 /**
  * Exporting server instance
  */
-export { sio };
+// export { sio };
 export default server;
